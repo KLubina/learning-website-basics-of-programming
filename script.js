7f2fe74385acb68677e73f048707c3f1e6549b1f
Kristian Lubina <kristian.lubina@outlook.com>
2025-02-06 19:28:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Define a function to output a message to the console
+function showMessage() {
+    console.log("Hello, this is a message from your JavaScript file!");
+}
+
+// Call the function to execute it
+showMessage();
